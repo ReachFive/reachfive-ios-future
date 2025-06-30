7ef2ce30505bfec508c54a122e31c097203ed2d9
François Devémy <francois.devemy@reach5.co>
2025-06-30 20:04:02 +0200

    Suppression de tout ce qui n'est pas une API

--- Sources/Core/Classes/models/requests/UpdatePhoneNumberRequest.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public class UpdatePhoneNumberRequest: Codable, DictionaryEncodable {
-    public let phoneNumber: String
-    
-    public init(phoneNumber: String) {
-        self.phoneNumber = phoneNumber
-    }
-}
--- Sources/Core/Classes/models/responses/webAuthn/CredentialCreationOptions.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public class CredentialCreationOptions: Codable, DictionaryEncodable {
-    public let publicKey: R5PublicKeyCredentialCreationOptions
-    
-    public init(publicKey: R5PublicKeyCredentialCreationOptions) {
-        self.publicKey = publicKey
-    }
-}
